d9ce1693e32571ed8cea7706c70bad873e7501b1
Devashish Singh Parmar <parmardevashish132@gmail.com>
2024-03-11 12:31:01 +0530

    Wokrking on delivery

--- src/Component/Home/SearchBar/SearchBar.jsx
+++ src/Component/Home/SearchBar/SearchBar.jsx
@@ -3,14 +3,14 @@ function SearchBar() {
         <div className="bg-white p-2 rounded-lg border-2 border-slate-200 shadow-lg h-12">
             <label>&#x1F50E;
                 <input
-                    className="border-black border-r-2 ml-1 focus:outline-none w-[20rem] "
+                    className="border-black border-r-2 ml-1 focus:outline-none w-[15rem] "
                     type="text"
                     placeholder="City"
                     spellCheck="false"
                 />
             </label>
             <input
-                className="ml-1 focus:outline-none w-[20rem]"
+                className="ml-1 focus:outline-none w-[15rem]"
                 type="text"
                 placeholder="Restaurant or dish"
                 spellCheck="false"
--- src/Component/OrderOnline/OrderOnline.jsx
+++ src/Component/OrderOnline/OrderOnline.jsx
@@ -5,15 +5,37 @@ function OrderOnline() {
     return (
         <>
             <div className="flex-1">
+
+                {/* Search bar */}
                 <div className="flex justify-center mt-4">
                     <p className="text-3xl text-black font-semibold mr-5">Zomiggy</p>
                     <SearchBar />
                 </div>
-                <div className="flex justify-center mt-4 ">
-                <div className="w-[50rem]">
-                    <Link className="w-24 ml-5 text-xl" to="/dining">Dining In</Link>
-                    <Link className="w-24 ml-5 text-xl" to="/order">Delivery</Link>
+
+                {/* Delivery or Dine In */}
+                <div className="flex justify-center mt-10">
+                    <div className="w-[50rem] flex">
+                        <Link className="mr-6 text-xl flex items-center w-40" to="/dining">
+                            <img className="w-10 rounded-full mr-2"
+                                src="images/delivery.png" alt="logo" />
+                            Dining In
+                        </Link>
+                        <Link className="text-xl flex items-center w-40" to="/order">
+                            <img className="rounded-full w-[3.3rem] mr-2"
+                                src="images/dinein.png" alt="logo" />
+                            Delivery
+                        </Link>
+                    </div>
                 </div>
+
+                {/* Options for menu */}
+                <div className="flex justify-center mt-5">
+                    <div className="w-[50rem]">
+                        <p className="text-2xl">Delivery Restuarants Near you</p>
+                        <div className="grid">
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
