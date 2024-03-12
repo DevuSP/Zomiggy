c8d47a6d01f327fd5d71f3066c7183835ea581c3
Devashish Singh Parmar <parmardevashish132@gmail.com>
2024-03-12 20:32:18 +0530

    JSON adding

---
+++ public/JSON/Food.jsx
@@ -0,0 +1,97 @@
+
+const delivery = {
+    "Restuarants": [
+        {
+            "Punjabi Dhaba": [
+                {
+                    "Dish": "Chole Bhature",
+                    "Delivery Time": "44 min",
+                    "Rating": "4.2",
+                    "Price": "150",
+                    "Image": "public/images/delivery/bhature.avif"
+                },
+                {
+                    "Dish": "Chicken Tikka Masala",
+                    "Delivery Time": "40 min",
+                    "Rating": "4.7",
+                    "Price": "130",
+                    "Image": "public/images/delivery/chickentikka.avif"
+                },
+                {
+                    "Dish": "Lemon Chicken",
+                    "Delivery Time": "42 min",
+                    "Rating": "4.8",
+                    "Price": "130",
+                    "Image": "public/images/delivery/lemonchicken.avif"
+                }
+            ]
+        },
+        {
+            "Snacks Point": [
+                {
+                    "Dish": "Momos",
+                    "Delivery Time": "23 min",
+                    "Rating": "4.3",
+                    "Price": "80",
+                    "Image": "public/images/delivery/momos.avif"
+                },
+                {
+                    "Dish": "Chowmin",
+                    "Delivery Time": "27 min",
+                    "Rating": "4.8",
+                    "Price": "80",
+                    "Image": "public/images/delivery/chowmin.png"
+                },
+                {
+                    "Dish": "Dosa",
+                    "Delivery Time": "32 min",
+                    "Rating": "4.8",
+                    "Price": "120",
+                    "Image": "public/images/delivery/dosa.jpg"
+                }
+            ]
+        }
+    ]
+}
+
+const restuarants = {
+    "Restuarants": [
+        {
+            "Name": "Centrum",
+            "Location": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi qui esse, modi sit necessitatibus eum.",
+            "Rating": "4.9",
+            "Image": "public/centrumhotel.avif"
+        },
+        {
+            "Name": "Bawarchi",
+            "Location": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi qui esse, modi sit necessitatibus eum.",
+            "Rating": "4.5",
+            "Image": "public/bawarchi.jpg"
+        },
+        {
+            "Name": "Chillis",
+            "Location": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi qui esse, modi sit necessitatibus eum.",
+            "Rating": "4.7",
+            "Image": "public/chillishotel.jpg"
+        },
+        {
+            "Name": "Kalsang",
+            "Location": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi qui esse, modi sit necessitatibus eum.",
+            "Rating": "4.7",
+            "Image": "public/kalsang.jpg"
+        },
+        {
+            "Name": "Raasta",
+            "Location": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi qui esse, modi sit necessitatibus eum.",
+            "Rating": "4.9",
+            "Image": "public/Raasta.jpg"
+        },
+        {
+            "Name": "Sayaji",
+            "Location": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi qui esse, modi sit necessitatibus eum.",
+            "Rating": "4.1",
+            "Image": "public/sayaji.jpg"
+        }
+    ]
+}
+export {delivery, restuarants};
