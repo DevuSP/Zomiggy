cf1c91fcf80d3412d697997dc37334b13856ae37
Devashish Singh Parmar <parmardevashish132@gmail.com>
2024-04-03 22:34:53 +0530

    Updates

--- JSON/Delivery.js
+++ JSON/Delivery.js
@@ -4,209 +4,209 @@
         "deliveryTime": "44 min",
         "rating": "4.2",
         "price": "150",
-        "image": "/images/delivery/bhature.avif"
+        "image": "/Zomiggy/images/delivery/bhature.avif"
     },
     {
         "dish": "Chicken Tikka Masala",
         "deliveryTime": "40 min",
         "rating": "4.7",
         "price": "130",
-        "image": "/images/delivery/chickentikka.avif"
+        "image": "/Zomiggy/images/delivery/chickentikka.avif"
     },
     {
         "dish": "Lemon Chicken",
         "deliveryTime": "42 min",
         "rating": "4.8",
         "price": "130",
-        "image": "/images/delivery/lemonchicken.png"
+        "image": "/Zomiggy/images/delivery/lemonchicken.png"
     },
     {
         "dish": "Momos",
         "deliveryTime": "23 min",
         "rating": "4.3",
         "price": "80",
-        "image": "/images/delivery/momos.avif"
+        "image": "/Zomiggy/images/delivery/momos.avif"
     },
     {
         "dish": "Chowmin",
         "deliveryTime": "27 min",
         "rating": "4.8",
         "price": "80",
-        "image": "/images/delivery/chowmin.png"
+        "image": "/Zomiggy/images/delivery/chowmin.png"
     },
     {
         "dish": "Dosa",
         "deliveryTime": "32 min",
         "rating": "4.8",
         "price": "120",
-        "image": "/images/delivery/dosa.jpg"
+        "image": "/Zomiggy/images/delivery/dosa.jpg"
     },
     {
         "dish": "Chole Bhature",
         "deliveryTime": "44 min",
         "rating": "4.2",
         "price": "150",
-        "image": "/images/delivery/bhature.avif"
+        "image": "/Zomiggy/images/delivery/bhature.avif"
     },
     {
         "dish": "Chicken Tikka Masala",
         "deliveryTime": "40 min",
         "rating": "4.7",
         "price": "130",
-        "image": "/images/delivery/chickentikka.avif"
+        "image": "/Zomiggy/images/delivery/chickentikka.avif"
     },
     {
         "dish": "Lemon Chicken",
         "deliveryTime": "42 min",
         "rating": "4.8",
         "price": "130",
-        "image": "/images/delivery/lemonchicken.png"
+        "image": "/Zomiggy/images/delivery/lemonchicken.png"
     },
     {
         "dish": "Momos",
         "deliveryTime": "23 min",
         "rating": "4.3",
         "price": "80",
-        "image": "/images/delivery/momos.avif"
+        "image": "/Zomiggy/images/delivery/momos.avif"
     },
     {
         "dish": "Chowmin",
         "deliveryTime": "27 min",
         "rating": "4.8",
         "price": "80",
-        "image": "/images/delivery/chowmin.png"
+        "image": "/Zomiggy/images/delivery/chowmin.png"
     },
     {
         "dish": "Dosa",
         "deliveryTime": "32 min",
         "rating": "4.8",
         "price": "120",
-        "image": "/images/delivery/dosa.jpg"
+        "image": "/Zomiggy/images/delivery/dosa.jpg"
     },
     {
         "dish": "Chole Bhature",
         "deliveryTime": "44 min",
         "rating": "4.2",
         "price": "150",
-        "image": "/images/delivery/bhature.avif"
+        "image": "/Zomiggy/images/delivery/bhature.avif"
     },
     {
         "dish": "Chicken Tikka Masala",
         "deliveryTime": "40 min",
         "rating": "4.7",
         "price": "130",
-        "image": "/images/delivery/chickentikka.avif"
+        "image": "/Zomiggy/images/delivery/chickentikka.avif"
     },
     {
         "dish": "Lemon Chicken",
         "deliveryTime": "42 min",
         "rating": "4.8",
         "price": "130",
-        "image": "/images/delivery/lemonchicken.png"
+        "image": "/Zomiggy/images/delivery/lemonchicken.png"
     },
     {
         "dish": "Momos",
         "deliveryTime": "23 min",
         "rating": "4.3",
         "price": "80",
-        "image": "/images/delivery/momos.avif"
+        "image": "/Zomiggy/images/delivery/momos.avif"
     },
     {
         "dish": "Chowmin",
         "deliveryTime": "27 min",
         "rating": "4.8",
         "price": "80",
-        "image": "/images/delivery/chowmin.png"
+        "image": "/Zomiggy/images/delivery/chowmin.png"
     },
     {
         "dish": "Dosa",
         "deliveryTime": "32 min",
         "rating": "4.8",
         "price": "120",
-        "image": "/images/delivery/dosa.jpg"
+        "image": "/Zomiggy/images/delivery/dosa.jpg"
     },
     {
         "dish": "Chole Bhature",
         "deliveryTime": "44 min",
         "rating": "4.2",
         "price": "150",
-        "image": "/images/delivery/bhature.avif"
+        "image": "/Zomiggy/images/delivery/bhature.avif"
     },
     {
         "dish": "Chicken Tikka Masala",
         "deliveryTime": "40 min",
         "rating": "4.7",
         "price": "130",
-        "image": "/images/delivery/chickentikka.avif"
+        "image": "/Zomiggy/images/delivery/chickentikka.avif"
     },
     {
         "dish": "Lemon Chicken",
         "deliveryTime": "42 min",
         "rating": "4.8",
         "price": "130",
-        "image": "/images/delivery/lemonchicken.png"
+        "image": "/Zomiggy/images/delivery/lemonchicken.png"
     },
     {
         "dish": "Momos",
         "deliveryTime": "23 min",
         "rating": "4.3",
         "price": "80",
-        "image": "/images/delivery/momos.avif"
+        "image": "/Zomiggy/images/delivery/momos.avif"
     },
     {
         "dish": "Chowmin",
         "deliveryTime": "27 min",
         "rating": "4.8",
         "price": "80",
-        "image": "/images/delivery/chowmin.png"
+        "image": "/Zomiggy/images/delivery/chowmin.png"
     },
     {
         "dish": "Dosa",
         "deliveryTime": "32 min",
         "rating": "4.8",
         "price": "120",
-        "image": "/images/delivery/dosa.jpg"
+        "image": "/Zomiggy/images/delivery/dosa.jpg"
     },
     {
         "dish": "Chole Bhature",
         "deliveryTime": "44 min",
         "rating": "4.2",
         "price": "150",
-        "image": "/images/delivery/bhature.avif"
+        "image": "/Zomiggy/images/delivery/bhature.avif"
     },
     {
         "dish": "Chicken Tikka Masala",
         "deliveryTime": "40 min",
         "rating": "4.7",
         "price": "130",
-        "image": "/images/delivery/chickentikka.avif"
+        "image": "/Zomiggy/images/delivery/chickentikka.avif"
     },
     {
         "dish": "Lemon Chicken",
         "deliveryTime": "42 min",
         "rating": "4.8",
         "price": "130",
-        "image": "/images/delivery/lemonchicken.png"
+        "image": "/Zomiggy/images/delivery/lemonchicken.png"
     },
     {
         "dish": "Momos",
         "deliveryTime": "23 min",
         "rating": "4.3",
         "price": "80",
-        "image": "/images/delivery/momos.avif"
+        "image": "/Zomiggy/images/delivery/momos.avif"
     },
     {
         "dish": "Chowmin",
         "deliveryTime": "27 min",
         "rating": "4.8",
         "price": "80",
-        "image": "/images/delivery/chowmin.png"
+        "image": "/Zomiggy/images/delivery/chowmin.png"
     },
     {
         "dish": "Dosa",
         "deliveryTime": "32 min",
         "rating": "4.8",
         "price": "120",
-        "image": "/images/delivery/dosa.jpg"
+        "image": "/Zomiggy/images/delivery/dosa.jpg"
     }
 ]
